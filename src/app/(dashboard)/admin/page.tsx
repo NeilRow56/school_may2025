e0dd4575eaebfc72019052e079a123e4f4351c2d
NeilRow56 <neilrowland56@gmail.com>
2025-05-22 09:13:12 +0100

    Added framework for admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,26 @@
 const AdminPage = () => {
-  return <div>Admin page</div>
+  return (
+    <div className='flex flex-col gap-4 p-4 md:flex-row'>
+      {/* LEFT */}
+      <div className='flex w-full flex-col gap-8 bg-yellow-50 lg:w-2/3'>
+        {/* USER CARDS */}
+        <div className='flex flex-wrap justify-between gap-4'>USER CARDS</div>
+        {/* MIDDLE CHARTS */}
+        <div className='flex flex-col gap-4 lg:flex-row'>
+          {/* COUNT CHART */}
+          <div className='h-[450px] w-full lg:w-1/3'>COUNT CHART</div>
+          {/* ATTENDANCE CHART */}
+          <div className='h-[450px] w-full lg:w-2/3'>ATTENDANCE CHART</div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className='h-[500px] w-full'>FINANCE CHART</div>
+      </div>
+      {/* RIGHT */}
+      <div className='flex w-full flex-col gap-8 bg-red-50 lg:w-1/3'>
+        EVENT CALENDAR ANNOUNCEMENTS
+      </div>
+    </div>
+  )
 }
 
 export default AdminPage
