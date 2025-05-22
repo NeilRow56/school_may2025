a12ec01e056639d8bd7ffb725a92fd9137f9ee47
NeilRow56 <neilrowland56@gmail.com>
2025-05-22 14:02:55 +0100

    Added student page with react big calendar

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,5 +1,24 @@
+import { Announcements } from '@/components/announcements'
+import BigCalendar from '@/components/big-calendar'
+import { EventCalendar } from '@/components/event-calendar'
+
 const StudentPage = () => {
-  return <div>Student page</div>
+  return (
+    <div className='flex flex-col gap-4 p-4 xl:flex-row'>
+      {/* LEFT */}
+      <div className='w-full xl:w-2/3'>
+        <div className='h-full rounded-md bg-white p-4'>
+          <h1 className='text-xl font-semibold'>Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className='flex w-full flex-col gap-8 xl:w-1/3'>
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  )
 }
 
 export default StudentPage
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,67 @@
 // TEMPORARY DATA
 
-export const role = 'admin'
+export const role = 'student'
+
+// YOU SHOULD CHANGE THE DATES OF THE EVENTS TO THE CURRENT DATE TO SEE THE EVENTS ON THE CALENDAR
+export const calendarEvents = [
+  {
+    title: 'Math',
+    allDay: false,
+    start: new Date('2025-05-19T08:30:00.000'),
+    end: new Date('2025-05-19T10:30:00.000')
+  },
+  {
+    title: 'English',
+    allDay: false,
+    start: new Date('2025-05-19T11:00:00.000'),
+    end: new Date('2025-05-19T12:30:00.000')
+  },
+  {
+    title: 'My Event',
+    allDay: false,
+    start: new Date('2025-05-19T14:00:00.000'),
+    end: new Date('2025-05-19T15:30:00.000')
+  },
+  {
+    title: 'Biology',
+    allDay: false,
+    start: new Date('2025-05-20T09:00:00.000'),
+    end: new Date('2025-05-20T12:30:00.000')
+  },
+  {
+    title: 'Physics',
+    allDay: false,
+    start: new Date('2025-05-20T14:00:00.000'),
+    end: new Date('2025-05-20T14:30:00.000')
+  },
+  {
+    title: 'Chemistry',
+    allDay: false,
+    start: new Date('2025-05-20T15:00:00.000'),
+    end: new Date('2025-05-20T16:30:00.000')
+  },
+  {
+    title: 'History',
+    allDay: false,
+    start: new Date('2025-05-21T08:00:00.000'),
+    end: new Date('2025-05-21T10:30:00.000')
+  },
+  {
+    title: 'English',
+    allDay: false,
+    start: new Date('2025-05-21T13:00:00.000'),
+    end: new Date('2025-05-21T15:30:00.000')
+  },
+  {
+    title: 'Biology',
+    allDay: true,
+    start: new Date('2025-05-22T13:00:00.000'),
+    end: new Date('2025-05-22T15:30:00.000')
+  },
+  {
+    title: 'Biology',
+    allDay: true,
+    start: new Date('2025-05-23'),
+    end: new Date('2025-05-23')
+  }
+]
